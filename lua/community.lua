1c47c8af00e32db60e603c7520e81760fcf63aae
Renato Semadeni <renato.semadeni@dentsplysirona.com>
2025-12-10 23:15:35 +0100

    fixed copilot integration

--- lua/community.lua
+++ lua/community.lua
@@ -1,17 +1,7 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  "AstroNvim/astrocommunity",
   { import = "astrocommunity.completion.copilot-lua" },
-  { import = "astrocommunity.ai.copilot-chat-nvim" }, -- Add this line
-  -- OR, if you want Copilot suggestions inside your main completion menu (popup):
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  -- import/override with your plugins folder
+  --  { import = "astrocommunity.ai.copilot-chat-nvim" },
 }
